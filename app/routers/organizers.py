516e59602b08422f802bcc5b2b95d8ce8170270c
Yannhrld-ngs <yannhrld@gmail.com>
2026-09-26 11:55:17 +0200

    reworking planning

--- app/routers/organizers.py
+++ app/routers/organizers.py
@@ -646,7 +646,7 @@ def envoyer_submit(
         f"En espérant vous revoir bientôt, \n{config.WEDDING_NAME1} & {config.WEDDING_NAME2}\n"
         "Dieu vous garde."
     )
-    contact = invite.contact.replace(" ", "").replace("+", "") or ""
+    contact = "" if invite.contact is None else invite.contact.replace(" ", "").replace("+", "")
 
     if canal == "whatsapp":
         url = f"https://wa.me/{contact}?text={quote(message)}"
